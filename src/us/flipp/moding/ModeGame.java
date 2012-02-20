3eadc3d02f31e807b94c25b4368f77fc84d2ad39
Bill Prin <waprin@gmail.com>
2012-02-20 10:33:55 -0500

    some basic board drawing complete

--- src/us/flipp/moding/ModeGame.java
+++ src/us/flipp/moding/ModeGame.java
@@ -9,6 +9,7 @@ import org.apache.http.cookie.Cookie;
 import us.flipp.simulation.World;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class ModeGame extends Mode {
 
@@ -46,6 +47,14 @@ public class ModeGame extends Mode {
     @Override
     public void redraw(Canvas canvas) {
         super.redraw(canvas);
+
+        Paint redPaint = new Paint();
+        redPaint.setARGB(255, 255, 0, 0);
+
+        Paint bluePaint = new Paint();
+        bluePaint.setARGB(255, 0, 0, 255);
+
+
         Paint blackRedraw = new Paint();
         blackRedraw.setARGB(255, 0, 0, 0);
         canvas.drawRect(new Rect(0, 0, canvas.getWidth(), canvas.getHeight()), blackRedraw);
@@ -53,13 +62,46 @@ public class ModeGame extends Mode {
         int widthBorder = canvas.getWidth() / 10;
         int heightBorder = canvas.getHeight() / 10;
 
+        int boardWidth = canvas.getWidth() - (2 * widthBorder);
+        int boardHeight = canvas.getHeight() - (2 * heightBorder);
 
-        Paint paint = new Paint();
-        paint.setARGB(255, 255, 0, 0);
+        int squareWidth = boardWidth / 10;
+        int squareHeight = boardHeight / 10;
+
+        int squareWidthBorder = squareWidth / 10;
+        int squareHeightBorder = squareHeight / 10;
+
+        int squareRectWidth = squareWidth - (squareWidthBorder * 2);
+        int squareRectHeight = squareHeight - (squareHeightBorder * 2);
+
+        World.Color[][] startBoard = world.getStartBoard();
+
+        for (int x = 0; x < world.getWidth(); x++) {
+            for (int y = 0; y < world.getHeight(); y++) {
+                World.Color color = startBoard[x][y];
+
+                Paint paint;
+
+                if (color == World.Color.BLUE) {
+                    paint = bluePaint;
+                }  else {
+                    paint = redPaint;
+                }
+
+                int leftOffset = widthBorder;
+                leftOffset += squareWidth * x;
+                leftOffset += squareWidthBorder;
+                int topOffset = heightBorder;
+                topOffset += squareHeight * y;
+                topOffset += squareHeightBorder;
+                canvas.drawRect(new Rect(leftOffset, topOffset, leftOffset + squareRectWidth, topOffset + squareRectHeight), paint);
+            }
+        }
+
         int halfX = canvas.getWidth() / 2;
         int halfY = canvas.getHeight() / 2;
-        canvas.drawRect(new Rect(0, 0, halfX,halfY), paint);
         String author = world.getAuthor() != null ? world.getAuthor() : "Anonymous";
-        canvas.drawText(author, 0, 300, paint);
+        canvas.drawText(author, 0, 300, redPaint);
+
     }
 }
